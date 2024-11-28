c68a52bbcdd4118f611ef7d90f6e16f7e9dcad41
Jefferson <jefferson.chileno@epn.edu.ec>
2024-11-28 15:23:24 -0500

    refactor(api-stock): manejo de errores de solicitud

--- js/services/stock-service.js
+++ js/services/stock-service.js
@@ -1,40 +1,45 @@
 export const fetchStockData = async (stockName, purchaseDate) => {
 	const apiKey = 'WIHWUHV5NNL5K90R'
 	const API_URL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockName}&apikey=${apiKey}&outputsize=full&datatype=csv`
-	const response = await fetch(API_URL)
-	const data = await response.text()
-	const rows = data.split('\n')
+	try {
+		const response = await fetch(API_URL)
+		const data = await response.text()
+		const rows = data.split('\n')
 
-	// Buscar la fecha de compra en los datos
-	let purchasePrice = null
-	let currentPrice = null
+		// Buscar la fecha de compra en los datos
+		let purchasePrice = null
+		let currentPrice = null
 
-	for (let row of rows) {
-		const [date, open, high, low, close, volume] = row.split(',')
+		for (let row of rows) {
+			const [date, open, high, low, close, volume] = row.split(',')
 
-		if (date && date === purchaseDate) {
-			purchasePrice = parseFloat(close)
-		}
-		if (date && new Date(date) <= new Date() && !currentPrice) {
-			currentPrice = parseFloat(close)
-		}
+			if (date && date === purchaseDate) {
+				purchasePrice = parseFloat(close)
+			}
+			if (date && new Date(date) <= new Date() && !currentPrice) {
+				currentPrice = parseFloat(close)
+			}
 
-		if (purchasePrice && currentPrice) {
-			// Salir del bucle si ambos precios se han encontrado
-			return {
-				purchasePrice,
-				currentPrice,
+			if (purchasePrice && currentPrice) {
+				// Salir del bucle si ambos precios se han encontrado
+				return {
+					purchasePrice,
+					currentPrice,
+				}
 			}
 		}
-	}
 
-	if (purchasePrice === null) {
-		alert('No se encontró el precio de la acción para la fecha de compra.')
-	}
+		if (purchasePrice === null) {
+			alert('No se encontró el precio de la acción para la fecha de compra.')
+		}
 
-	if (currentPrice === null) {
-		alert('No se pudo obtener el precio actual de la acción.')
-	}
+		if (currentPrice === null) {
+			alert('No se pudo obtener el precio actual de la acción.')
+		}
 
-	return null
+		return null
+	} catch (error) {
+		console.error('Error al realizar la solicitud:', error)
+		alert('Hubo un error al obtener los datos. Intenta de nuevo más tarde.')
+	}
 }
