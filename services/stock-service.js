1f5c7228a850aad3a1cad57714d9b1bc3394e428
Jefferson <jefferson.chileno@epn.edu.ec>
2025-02-03 17:03:57 -0500

    feat(api-stock): actualización de token y funcion de consulta de acciones

--- services/stock-service.js
+++ services/stock-service.js
@@ -1,36 +1,66 @@
+const apiKey = 'PHPXRNPVIXXAMWWX';
+
 export async function fetchStockData(stockName, purchaseDate) {
-	const apiKey = 'WIHWUHV5NNL5K90R'; // Reemplaza con tu clave de API de Alpha Vantage
-  
 	try {
-	  const response = await fetch(
-		`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockName}&apikey=${apiKey}&outputsize=full&datatype=csv`
-	  );
-  
-	  const data = await response.text();
-	  const rows = data.split('\n');
-  
-	  let purchasePrice = null;
-	  let currentPrice = null;
-  
-	  for (let row of rows) {
-		const [date, open, high, low, close, volume] = row.split(',');
-  
-		if (date && date === purchaseDate) {
-		  purchasePrice = parseFloat(close);
+		const response = await fetch(
+			`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockName}&apikey=${apiKey}&outputsize=full&datatype=csv`
+		);
+
+		const data = await response.text();
+		const rows = data.split('\n');
+
+		let purchasePrice = null;
+		let currentPrice = null;
+
+		for (let row of rows) {
+			const [date, open, high, low, close, volume] = row.split(',');
+
+			if (date && date === purchaseDate) {
+				purchasePrice = parseFloat(close);
+			}
+			if (date && new Date(date) <= new Date() && !currentPrice) {
+				currentPrice = parseFloat(close);
+			}
+
+			if (purchasePrice && currentPrice) {
+				break;
+			}
 		}
-		if (date && new Date(date) <= new Date() && !currentPrice) {
-		  currentPrice = parseFloat(close);
-		}
-  
-		if (purchasePrice && currentPrice) {
-		  break;
-		}
-	  }
-  
-	  return { purchasePrice, currentPrice };
+
+		return { purchasePrice, currentPrice };
 	} catch (error) {
-	  console.error('Error al realizar la solicitud:', error);
-	  throw new Error('Hubo un error al obtener los datos.');
+		console.error('Error al realizar la solicitud:', error);
+		throw new Error('Hubo un error al obtener los datos.');
 	}
-  }
-  
+}
+
+export async function fetchCurrentPrice(stockName) {
+	try {
+		const response = await fetch(
+			`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockName}&apikey=${apiKey}&outputsize=full&datatype=csv`
+		);
+
+		const data = await response.text();
+		const rows = data.split('\n');
+
+		let currentPrice = null;
+		console.log(rows);
+
+		for (let row of rows) {
+			const [date, open, high, low, close, volume] = row.split(',');
+
+			if (date && new Date(date) <= new Date() && !currentPrice) {
+				currentPrice = parseFloat(close);
+			}
+
+			if (currentPrice) {
+				break;
+			}
+		}
+
+		return currentPrice;
+	} catch (error) {
+		console.error('Error al realizar la solicitud:', error);
+		throw new Error('Hubo un error al obtener los datos.');
+	}
+}
